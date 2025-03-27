a83732f6a97157157d2980a7e9a543eb84f254c0
Qwerty4254 <ahmedhamdouni122@gmail.com>
2025-03-27 21:53:21 +0000

    first version ended

--- main.c
+++ main.c
@@ -16,18 +16,40 @@
 */
 
 #include <stdio.h>
+#include <windows.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include "file_manipulation.c"
 
 int add_user_fun();
-int find_comp();
+int find_donnor();
+int find_reciver();
 int list_user();
-int valid_name(char *s);
+int valid_name();
 int valid_age();
 int valid_type();
 
 char *types[8]={"ab+","ab-","a+","a-","b+","b-","o+","o-"};
+char *donnor[][8]={
+  {"ab+", "ab-", "a+", "a-", "b+", "b-", "o+", "o-"},
+  {"ab-", "a-", "b-", "o-", NULL, NULL, NULL, NULL},
+  {"a+", "a-", "o+", "o-", NULL, NULL, NULL, NULL},
+  {"a-", "o-", NULL, NULL, NULL, NULL, NULL, NULL},
+  {"b+", "b-", "o+", "o-", NULL, NULL, NULL, NULL},
+  {"b-", "o-", NULL, NULL, NULL, NULL, NULL, NULL},
+  {"o+", "o-", NULL, NULL, NULL, NULL, NULL, NULL},
+  {"o-", NULL, NULL, NULL, NULL, NULL, NULL, NULL}
+};
+char *reciver[][8]={
+  {"ab+",  NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+  {"ab+", "ab-", NULL, NULL, NULL, NULL, NULL, NULL},
+  {"ab+", "a+", NULL, NULL, NULL, NULL, NULL, NULL},
+  {"ab+", "ab-", "a+", "a-", NULL, NULL, NULL, NULL},
+  {"ab+", "b+", NULL, NULL, NULL, NULL, NULL, NULL},
+  {"ab+", "ab-", "b+", "b-", NULL, NULL, NULL, NULL},
+  {"ab+", "a+", "b+", "o+", NULL, NULL, NULL, NULL},
+  {"ab+", "ab-", "a+", "a-", "b+", "b-", "o+", "o-"},
+};
 
 /*
 +-----+-----------------------+
@@ -54,30 +76,37 @@ char *types[8]={"ab+","ab-","a+","a-","b+","b-","o+","o-"};
 +---------+-----------------------------------------+---------+---------+
 */
 
-
-
+/*
+  for (int i = 0; i < 8; i++)
+  {
+    printf("%s ",*(acquire[4]+i));
+  }
+*/
 
 int main(){
   while (1)
   {
     int size=MAX_LINE;
-    char *x=(char*)malloc(size*sizeof(char));
-    char *ptr;
+    char *x=(char*)malloc(size*sizeof(char)),*ptr;
     if (x == NULL) {
       perror("Memory allocation failed");
       return 1;
     }
 
-    printf("+-----+-----------------------+"
+    printf(
+  "+-----+-----------------------+"
   "\n|  1  | exit                  |"
   "\n+-----+-----------------------+"
   "\n|  2  | add user              |"
   "\n+-----+-----------------------+"
-  "\n|  3  | find compatible user  |"
+  "\n|  3  | find a donnor user    |"
   "\n+-----+-----------------------+"
-  "\n|  4  | list user             |"
+  "\n|  4  | find a reciver user   |"
+  "\n+-----+-----------------------+"
+  "\n|  5  | list user             |"
   "\n+-----+-----------------------+"
   "\n\n>");
+
 
     while (1){
       int index=0;
@@ -95,29 +124,45 @@ int main(){
         x[index++]=tolower(c);
       }
       x[index++]='\0';
-      if (strtol(x,&ptr,10)==1 && *ptr=='\0' || strcmp(x,"exit")==0) {free(x);printf("Exiting"); return 0;}
+      if (strtol(x,&ptr,10)==1 && *ptr=='\0' || strcmp(x,"exit")==0) {free(x);printf("Exiting");Sleep(1000); return 0;}
       else if (strtol(x,&ptr,10)==2  && *ptr=='\0'){
         free(x);
         if(!check_first_line()) make_header();
         add_user_fun();
         break;
       }
-      else if (strtol(x,&ptr,10)==3  && *ptr=='\0'){continue;}
-      else if (strtol(x,&ptr,10)==4  && *ptr=='\0'){continue;}
+      else if (strtol(x,&ptr,10)==3  && *ptr=='\0'){
+        free(x);
+        find_donnor();
+        break;
+      }
+      else if (strtol(x,&ptr,10)==4  && *ptr=='\0'){
+        free(x);
+        find_reciver();
+        break;
+      }
+      else if (strtol(x,&ptr,10)==5  && *ptr=='\0'){
+        free(x);
+        if(!check_first_line() || get_lines()==0) {printf("\nno user found\n\n");make_header();break;};
+        list_user();
+        break;
+      }
     }
   }
   return 0;
 }
 
 int valid_name(char *s){
-  while (*s)
-  {
-    if(!isalpha(tolower(*s)) && *s!=' '){
-      return 1;
-    }
-    s++;
+  int has_letter = 1;
+
+  for (int i = 0; s[i] != '\0'; i++) {
+      if (isalpha(s[i])) {
+          has_letter = 0;
+      } else if (!isspace(s[i])) {
+          return 1;
+      }
   }
-  return 0;
+  return has_letter;
 }
 
 int valid_age(char *s){
@@ -154,8 +199,8 @@ int valid_type(char *s){
 // main funcs
 
 int add_user_fun(){
-  char name[50],age[50],type[50];
-  char *b=(char*)malloc(50*sizeof(char)); // user input
+  char name[40],age[40],type[40];
+  char *b=(char*)malloc(40*sizeof(char)); // user input
   if (b == NULL) {
     perror("Memory allocation failed");
     return -1;
@@ -163,21 +208,24 @@ int add_user_fun(){
   do
   {
     printf("enter a name> ");
-    scanf("%s",b);
-  } while (valid_name(b) || strlen(b)>=50); // see if the user type more than 50 char or if the name is valid
+    fgets(b, 40, stdin);
+    b[strcspn(b, "\n")] = '\0';
+  } while (valid_name(b) || strlen(b)>=40); // see if the user type more than 50 char or if the name is valid
   strcpy(name,b); // copy the input text to the variable 
 
   do
   {
     printf("enter a age> ");
-    scanf("%s",b);
-  } while (valid_age(b) || strlen(b)>=50);strcpy(age,b);
+    fgets(b, 40, stdin);
+    b[strcspn(b, "\n")] = '\0';
+  } while (valid_age(b) || strlen(b)>=40);strcpy(age,b);
 
   do
   {
     printf("enter a blood type> ");
-    scanf("%s",b);
-  } while (valid_type(b) || strlen(b)>=50);strcpy(type,b);
+    fgets(b, 40, stdin);
+    b[strcspn(b, "\n")] = '\0';
+  } while (valid_type(b) || strlen(b)>=40);strcpy(type,b);
 
   free(b);
   int user=add_user(name,age,type);
@@ -189,3 +237,116 @@ int add_user_fun(){
   return user;
 }
 
+int list_user(){
+  printf("\n\n%42s\n","USER LIST");
+  for (int i = 0; i < get_lines(); i++)
+  {
+    printf(
+      "+---------+-----------------------------------------+---------+---------+\n"
+      "|    %-5d| %-40s|   %-6s|   %-6s|\n",i,get_name(i),get_age(i),get_type(i));
+  }
+  printf("+---------+-----------------------------------------+---------+---------+\n\n");
+  Sleep(500);
+}
+
+int find_donnor(){
+  if(!check_first_line() || get_lines()==0) {
+    printf("\nno user found\n\n");
+    make_header();
+    return -1;
+  }
+  char *b=(char *)malloc(MAX_LINE*sizeof(char)),*endptr,*type;
+  int pos,index,count=0;
+  do{
+    printf("enter the user id> ");
+    fgets(b, 40, stdin);
+    b[strcspn(b,"\n")]='\0';
+    pos=(int)strtol(b,&endptr,10);
+  }while(strlen(b)>100 || strlen(b)<=0 || pos<0 || pos>=get_lines() || *endptr!='\0');
+  free(b);
+  for (int i=0 ;i < 8; i++){
+    if(strcmp(types[i],get_type(pos))==0){
+      index=i;
+      break;
+    }
+  }
+  printf("\n\n%42s\n","DONNER LIST");
+  for (int i = 0; i < get_lines(); i++){
+    if(pos==i){
+      continue;
+    }
+    for (int j=0; j<8; j++){
+      if (*(donnor[index]+j)==NULL) continue;
+      if(strcmp(*(donnor[index]+j),get_type(i))==0){
+        count++;
+        printf(
+          "+---------+-----------------------------------------+---------+---------+\n"
+          "|    %-5d| %-40s|   %-6s|   %-6s|\n",i,get_name(i),get_age(i),get_type(i)
+        );
+        break;
+      }
+    }
+  }
+  if (count!=0){
+    printf("+---------+-----------------------------------------+---------+---------+\n\n");
+  }else{
+    printf(
+      "+---------+-----------------------------------------+---------+---------+\n"
+      "+                             NO USER FOUND                             +\n"
+      "+---------+-----------------------------------------+---------+---------+\n\n"
+    );
+  }
+  Sleep(1000);
+  return 0;
+}
+
+int find_reciver(){
+  if(!check_first_line() || get_lines()==0) {
+    printf("\nno user found\n\n");
+    make_header();
+    return -1;
+  }
+  char *b=(char *)malloc(MAX_LINE*sizeof(char)),*endptr,*type;
+  int pos,index,count=0;
+  do{
+    printf("enter the user id> ");
+    fgets(b, 40, stdin);
+    b[strcspn(b,"\n")]='\0';
+    pos=(int)strtol(b,&endptr,10);
+  }while(strlen(b)>100 || strlen(b)<=0 || pos<0 || pos>=get_lines() || *endptr!='\0');
+  free(b);
+  for (int i=0 ;i < 8; i++){
+    if(strcmp(types[i],get_type(pos))==0){
+      index=i;
+      break;
+    }
+  }
+  printf("\n\n%42s\n","DONNER LIST");
+  for (int i = 0; i < get_lines(); i++){
+    if(pos==i){
+      continue;
+    }
+    for (int j=0; j<8; j++){
+      if (*(reciver[index]+j)==NULL) continue;
+      if(strcmp(*(reciver[index]+j),get_type(i))==0){
+        count++;
+        printf(
+          "+---------+-----------------------------------------+---------+---------+\n"
+          "|    %-5d| %-40s|   %-6s|   %-6s|\n",i,get_name(i),get_age(i),get_type(i)
+        );
+        break;
+      }
+    }
+  }
+  if (count!=0){
+    printf("+---------+-----------------------------------------+---------+---------+\n\n");
+  }else{
+    printf(
+      "+---------+-----------------------------------------+---------+---------+\n"
+      "+                             NO USER FOUND                             +\n"
+      "+---------+-----------------------------------------+---------+---------+\n\n"
+    );
+  }
+  Sleep(1000);
+  return 0;
+}
